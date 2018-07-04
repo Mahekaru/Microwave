f349b4e5d1ae0afc929c4d07cbfeb98bff193116
Unknown <Mahekaru@yahoo.com>
2018-07-04 02:34:31 -0400

    Adding application and resource files

---
+++ Microwave/Microwave.Designer.vb
@@ -0,0 +1,284 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMicrowave
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.lblTimer = New System.Windows.Forms.Label()
+        Me.pnlOne = New System.Windows.Forms.Panel()
+        Me.pnlOpen = New System.Windows.Forms.Panel()
+        Me.tmrTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlTwo = New System.Windows.Forms.Panel()
+        Me.pnlThree = New System.Windows.Forms.Panel()
+        Me.pnlFour = New System.Windows.Forms.Panel()
+        Me.pnlFive = New System.Windows.Forms.Panel()
+        Me.pnlSix = New System.Windows.Forms.Panel()
+        Me.pnlSeven = New System.Windows.Forms.Panel()
+        Me.pnlEight = New System.Windows.Forms.Panel()
+        Me.pnlNine = New System.Windows.Forms.Panel()
+        Me.pnlZero = New System.Windows.Forms.Panel()
+        Me.pnlStart = New System.Windows.Forms.Panel()
+        Me.pnlCancel = New System.Windows.Forms.Panel()
+        Me.pnlOpenMicrowave = New System.Windows.Forms.Panel()
+        Me.pnlThirtySec = New System.Windows.Forms.Panel()
+        Me.pnlMinute = New System.Windows.Forms.Panel()
+        Me.tmrTime = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrDelay = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'lblTimer
+        '
+        Me.lblTimer.BackColor = System.Drawing.Color.Transparent
+        Me.lblTimer.Font = New System.Drawing.Font("Arial Narrow", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTimer.Location = New System.Drawing.Point(370, 92)
+        Me.lblTimer.Name = "lblTimer"
+        Me.lblTimer.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.lblTimer.Size = New System.Drawing.Size(84, 18)
+        Me.lblTimer.TabIndex = 3
+        Me.lblTimer.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'pnlOne
+        '
+        Me.pnlOne.BackColor = System.Drawing.Color.Transparent
+        Me.pnlOne.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlOne.Location = New System.Drawing.Point(396, 170)
+        Me.pnlOne.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlOne.Name = "pnlOne"
+        Me.pnlOne.Size = New System.Drawing.Size(18, 17)
+        Me.pnlOne.TabIndex = 2
+        '
+        'pnlOpen
+        '
+        Me.pnlOpen.BackColor = System.Drawing.Color.Transparent
+        Me.pnlOpen.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlOpen.Location = New System.Drawing.Point(386, 278)
+        Me.pnlOpen.Name = "pnlOpen"
+        Me.pnlOpen.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.pnlOpen.Size = New System.Drawing.Size(63, 28)
+        Me.pnlOpen.TabIndex = 3
+        '
+        'tmrTimer
+        '
+        Me.tmrTimer.Interval = 1000
+        '
+        'pnlTwo
+        '
+        Me.pnlTwo.BackColor = System.Drawing.Color.Transparent
+        Me.pnlTwo.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlTwo.Location = New System.Drawing.Point(414, 170)
+        Me.pnlTwo.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlTwo.Name = "pnlTwo"
+        Me.pnlTwo.Size = New System.Drawing.Size(18, 17)
+        Me.pnlTwo.TabIndex = 4
+        '
+        'pnlThree
+        '
+        Me.pnlThree.BackColor = System.Drawing.Color.Transparent
+        Me.pnlThree.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlThree.Location = New System.Drawing.Point(432, 170)
+        Me.pnlThree.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlThree.Name = "pnlThree"
+        Me.pnlThree.Size = New System.Drawing.Size(18, 17)
+        Me.pnlThree.TabIndex = 5
+        '
+        'pnlFour
+        '
+        Me.pnlFour.BackColor = System.Drawing.Color.Transparent
+        Me.pnlFour.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlFour.Location = New System.Drawing.Point(396, 187)
+        Me.pnlFour.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlFour.Name = "pnlFour"
+        Me.pnlFour.Size = New System.Drawing.Size(18, 17)
+        Me.pnlFour.TabIndex = 6
+        '
+        'pnlFive
+        '
+        Me.pnlFive.BackColor = System.Drawing.Color.Transparent
+        Me.pnlFive.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlFive.Location = New System.Drawing.Point(414, 187)
+        Me.pnlFive.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlFive.Name = "pnlFive"
+        Me.pnlFive.Size = New System.Drawing.Size(18, 17)
+        Me.pnlFive.TabIndex = 7
+        '
+        'pnlSix
+        '
+        Me.pnlSix.BackColor = System.Drawing.Color.Transparent
+        Me.pnlSix.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlSix.Location = New System.Drawing.Point(432, 187)
+        Me.pnlSix.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlSix.Name = "pnlSix"
+        Me.pnlSix.Size = New System.Drawing.Size(18, 17)
+        Me.pnlSix.TabIndex = 8
+        '
+        'pnlSeven
+        '
+        Me.pnlSeven.BackColor = System.Drawing.Color.Transparent
+        Me.pnlSeven.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlSeven.Location = New System.Drawing.Point(396, 204)
+        Me.pnlSeven.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlSeven.Name = "pnlSeven"
+        Me.pnlSeven.Size = New System.Drawing.Size(18, 26)
+        Me.pnlSeven.TabIndex = 9
+        '
+        'pnlEight
+        '
+        Me.pnlEight.BackColor = System.Drawing.Color.Transparent
+        Me.pnlEight.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlEight.Location = New System.Drawing.Point(414, 204)
+        Me.pnlEight.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlEight.Name = "pnlEight"
+        Me.pnlEight.Size = New System.Drawing.Size(18, 19)
+        Me.pnlEight.TabIndex = 10
+        '
+        'pnlNine
+        '
+        Me.pnlNine.BackColor = System.Drawing.Color.Transparent
+        Me.pnlNine.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlNine.Location = New System.Drawing.Point(432, 204)
+        Me.pnlNine.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlNine.Name = "pnlNine"
+        Me.pnlNine.Size = New System.Drawing.Size(18, 26)
+        Me.pnlNine.TabIndex = 11
+        '
+        'pnlZero
+        '
+        Me.pnlZero.BackColor = System.Drawing.Color.Transparent
+        Me.pnlZero.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlZero.Location = New System.Drawing.Point(414, 223)
+        Me.pnlZero.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlZero.Name = "pnlZero"
+        Me.pnlZero.Size = New System.Drawing.Size(18, 20)
+        Me.pnlZero.TabIndex = 12
+        '
+        'pnlStart
+        '
+        Me.pnlStart.BackColor = System.Drawing.Color.Transparent
+        Me.pnlStart.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlStart.Location = New System.Drawing.Point(452, 149)
+        Me.pnlStart.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlStart.Name = "pnlStart"
+        Me.pnlStart.Size = New System.Drawing.Size(18, 17)
+        Me.pnlStart.TabIndex = 13
+        '
+        'pnlCancel
+        '
+        Me.pnlCancel.BackColor = System.Drawing.Color.Transparent
+        Me.pnlCancel.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlCancel.Location = New System.Drawing.Point(452, 180)
+        Me.pnlCancel.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlCancel.Name = "pnlCancel"
+        Me.pnlCancel.Size = New System.Drawing.Size(18, 17)
+        Me.pnlCancel.TabIndex = 14
+        '
+        'pnlOpenMicrowave
+        '
+        Me.pnlOpenMicrowave.BackColor = System.Drawing.Color.Transparent
+        Me.pnlOpenMicrowave.Location = New System.Drawing.Point(51, 63)
+        Me.pnlOpenMicrowave.Name = "pnlOpenMicrowave"
+        Me.pnlOpenMicrowave.Size = New System.Drawing.Size(316, 262)
+        Me.pnlOpenMicrowave.TabIndex = 15
+        Me.pnlOpenMicrowave.Visible = False
+        '
+        'pnlThirtySec
+        '
+        Me.pnlThirtySec.BackColor = System.Drawing.Color.Transparent
+        Me.pnlThirtySec.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlThirtySec.Location = New System.Drawing.Point(432, 223)
+        Me.pnlThirtySec.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlThirtySec.Name = "pnlThirtySec"
+        Me.pnlThirtySec.Size = New System.Drawing.Size(18, 20)
+        Me.pnlThirtySec.TabIndex = 12
+        '
+        'pnlMinute
+        '
+        Me.pnlMinute.BackColor = System.Drawing.Color.Transparent
+        Me.pnlMinute.ForeColor = System.Drawing.Color.Transparent
+        Me.pnlMinute.Location = New System.Drawing.Point(396, 223)
+        Me.pnlMinute.Margin = New System.Windows.Forms.Padding(0)
+        Me.pnlMinute.Name = "pnlMinute"
+        Me.pnlMinute.Size = New System.Drawing.Size(18, 20)
+        Me.pnlMinute.TabIndex = 16
+        '
+        'tmrTime
+        '
+        Me.tmrTime.Enabled = True
+        '
+        'tmrDelay
+        '
+        Me.tmrDelay.Interval = 1000
+        '
+        'frmMicrowave
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Microwave.My.Resources.Resources.Microwave_Closed
+        Me.ClientSize = New System.Drawing.Size(493, 357)
+        Me.Controls.Add(Me.pnlZero)
+        Me.Controls.Add(Me.pnlMinute)
+        Me.Controls.Add(Me.pnlThirtySec)
+        Me.Controls.Add(Me.pnlCancel)
+        Me.Controls.Add(Me.pnlOpenMicrowave)
+        Me.Controls.Add(Me.pnlStart)
+        Me.Controls.Add(Me.pnlNine)
+        Me.Controls.Add(Me.pnlEight)
+        Me.Controls.Add(Me.pnlSeven)
+        Me.Controls.Add(Me.pnlSix)
+        Me.Controls.Add(Me.pnlFive)
+        Me.Controls.Add(Me.pnlFour)
+        Me.Controls.Add(Me.pnlThree)
+        Me.Controls.Add(Me.pnlTwo)
+        Me.Controls.Add(Me.pnlOne)
+        Me.Controls.Add(Me.pnlOpen)
+        Me.Controls.Add(Me.lblTimer)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MaximizeBox = False
+        Me.Name = "frmMicrowave"
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "Microwave"
+        Me.TransparencyKey = System.Drawing.Color.FromArgb(CType(CType(237, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(36, Byte), Integer))
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblTimer As System.Windows.Forms.Label
+    Friend WithEvents pnlOne As System.Windows.Forms.Panel
+    Friend WithEvents pnlOpen As System.Windows.Forms.Panel
+    Friend WithEvents pnlTwo As System.Windows.Forms.Panel
+    Friend WithEvents pnlThree As System.Windows.Forms.Panel
+    Friend WithEvents pnlFour As System.Windows.Forms.Panel
+    Friend WithEvents pnlFive As System.Windows.Forms.Panel
+    Friend WithEvents pnlSix As System.Windows.Forms.Panel
+    Friend WithEvents pnlSeven As System.Windows.Forms.Panel
+    Friend WithEvents pnlEight As System.Windows.Forms.Panel
+    Friend WithEvents pnlNine As System.Windows.Forms.Panel
+    Friend WithEvents pnlZero As System.Windows.Forms.Panel
+    Friend WithEvents pnlStart As System.Windows.Forms.Panel
+    Friend WithEvents pnlCancel As System.Windows.Forms.Panel
+    Friend WithEvents tmrTimer As System.Windows.Forms.Timer
+    Friend WithEvents pnlOpenMicrowave As System.Windows.Forms.Panel
+    Friend WithEvents pnlThirtySec As System.Windows.Forms.Panel
+    Friend WithEvents pnlMinute As System.Windows.Forms.Panel
+    Public WithEvents tmrTime As System.Windows.Forms.Timer
+    Friend WithEvents tmrDelay As System.Windows.Forms.Timer
+
+End Class
---
+++ Microwave/Microwave.vb
@@ -0,0 +1,318 @@
+﻿Public Class frmMicrowave
+    Private timer As String = ""
+    Private minute As Integer
+    Private second As Integer
+    Private Open As Integer
+    Private Start As Integer
+    Private delay As Integer
+
+    Private Sub pnlOne_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlOne.MouseClick
+        displayTime(1)
+    End Sub
+
+    Private Sub pnlTwo_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlTwo.MouseClick
+        displayTime(2)
+    End Sub
+
+    Private Sub pnlThree_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlThree.MouseClick
+        displayTime(3)
+    End Sub
+
+    Private Sub pnlFour_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlFour.MouseClick
+        displayTime(4)
+    End Sub
+
+    Private Sub pnlFive_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlFive.MouseClick
+        displayTime(5)
+    End Sub
+
+    Private Sub pnlSix_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlSix.MouseClick
+        displayTime(6)
+    End Sub
+
+    Private Sub pnlSeven_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlSeven.MouseClick
+        displayTime(7)
+    End Sub
+
+    Private Sub pnlEight_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlEight.MouseClick
+        displayTime(8)
+    End Sub
+
+    Private Sub pnlNine_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlNine.MouseClick
+        displayTime(9)
+    End Sub
+
+    Private Sub pnlZero_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlZero.MouseClick
+        displayTime(0)
+    End Sub
+
+    Private Sub pnlThirtySec_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlThirtySec.MouseClick
+        'If Val(timer) >= "9970" Then
+        '    My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+
+        'Else
+        '    My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+        '    timer = Val(timer) + "30"
+        '    displayTime(timer)
+        'End If
+
+    End Sub
+    Private Sub pnlMinute_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlMinute.MouseClick
+        'If Val(timer) >= "9900" Then
+        '    My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+        'Else
+        '    My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+        '    timer = Val(timer) + "100"
+        '    displayTime(timer)
+        'End If
+    End Sub
+
+    Sub displayTime(tmr)
+        Dim display As String
+        Dim distime As Integer
+        distime = CInt(timer)
+
+        distime += tmr
+
+        Select Case timer.Count
+            Case 4
+
+            Case 3
+
+            Case 2
+
+            Case Else
+        End Select
+
+        'If timer.Length > 4 Then
+        '    timer = timer.Substring(0, 4)
+        'End If
+
+        'display = timer.PadLeft(4, "0"c)
+
+        'second = CInt(display.Substring(2))
+        'minute = CInt(display.Substring(0, 2))
+
+        'lblTimer.Text = String.Format("{0:D2}:{1:D2}", minute, second)
+        'My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+    End Sub
+    Sub VisibleOn()
+
+        pnlStart.Visible = True
+        pnlOne.Visible = True
+        pnlTwo.Visible = True
+        pnlThree.Visible = True
+        pnlFour.Visible = True
+        pnlFive.Visible = True
+        pnlSix.Visible = True
+        pnlSeven.Visible = True
+        pnlEight.Visible = True
+        pnlNine.Visible = True
+        pnlZero.Visible = True
+        pnlThirtySec.Visible = True
+        pnlMinute.Visible = True
+        pnlStart.Visible = True
+        pnlCancel.Visible = True
+        pnlOpen.Visible = True
+    End Sub
+    Sub VisibleOff()
+
+        pnlOne.Visible = False
+        pnlTwo.Visible = False
+        pnlThree.Visible = False
+        pnlFour.Visible = False
+        pnlFive.Visible = False
+        pnlSix.Visible = False
+        pnlSeven.Visible = False
+        pnlEight.Visible = False
+        pnlNine.Visible = False
+        pnlZero.Visible = False
+        pnlThirtySec.Visible = False
+        pnlMinute.Visible = False
+        pnlStart.Visible = False
+        'pnlCancel.Visible = False
+
+    End Sub
+
+    Private Sub pnlStart_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlStart.MouseClick
+        If Start = 1 Then
+            My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+            My.Computer.Audio.Play(My.Resources._Loop, AudioPlayMode.BackgroundLoop)
+            Me.BackgroundImage = My.Resources.Microwave_On
+
+            tmrTimer.Enabled = True
+            tmrTime.Enabled = False
+            VisibleOff()
+
+            Start -= 1
+
+        Else
+            tmrTime.Enabled = False
+            VisibleOff()
+
+            If minute > 0 Then
+                Start = 1
+
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources._Loop, AudioPlayMode.BackgroundLoop)
+                Me.BackgroundImage = My.Resources.Microwave_On
+                timer = timer.PadLeft(4, "0"c)
+
+                second = CInt(timer.Substring(2))
+                minute = CInt(timer.Substring(0, 2))
+
+                lblTimer.Text = String.Format("{0:d2}:{1:d2}", minute, second)
+
+                timer = ""
+                tmrTimer.Enabled = True
+            ElseIf second > 0 Then
+                Start = 1
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources._Loop, AudioPlayMode.BackgroundLoop)
+                Me.BackgroundImage = My.Resources.Microwave_On
+                timer = timer.PadLeft(4, "0"c)
+
+                second = CInt(timer.Substring(2))
+                minute = CInt(timer.Substring(0, 2))
+
+                lblTimer.Text = String.Format("{0:d2}:{1:d2}", minute, second)
+
+                timer = ""
+                tmrTimer.Enabled = True
+            Else
+
+                tmrTime.Enabled = True
+
+                VisibleOn()
+
+                Start = 0
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+            End If
+        End If
+    End Sub
+
+    Private Sub pnlCancel_MouseClick(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlCancel.MouseClick
+        timer = ""
+        Start = 0
+        Open = 0
+        minute = 0
+        second = 0
+        tmrTimer.Enabled = False
+
+        Me.BackgroundImage = My.Resources.Microwave_Closed
+        My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.Background)
+
+        tmrTime.Enabled = True
+
+        VisibleOn()
+    End Sub
+
+    Private Sub pnlOpen_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles pnlOpen.Click
+        Me.BackgroundImage = My.Resources.Microwave_Open
+        My.Computer.Audio.Play(My.Resources.DoorOpen, AudioPlayMode.Background)
+
+        VisibleOff()
+
+        tmrTimer.Enabled = False
+        pnlOpenMicrowave.Visible = True
+
+        pnlCancel.Visible = False
+        Open = 1
+    End Sub
+
+    Private Sub pnlOpenMicrowave_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles pnlOpenMicrowave.Click
+        If Open = 1 Then
+            Open -= 1
+            pnlCancel.Visible = True
+        End If
+
+        If Start > 0 Then
+            If minute > 0 Then
+                Me.BackgroundImage = My.Resources.Microwave_Closed
+                My.Computer.Audio.Play(My.Resources.DoorClose, AudioPlayMode.Background)
+
+                VisibleOff()
+
+                pnlStart.Visible = True
+                pnlOpenMicrowave.Visible = False
+
+            ElseIf second > 0 Then
+
+                Me.BackgroundImage = My.Resources.Microwave_Closed
+                My.Computer.Audio.Play(My.Resources.DoorClose, AudioPlayMode.Background)
+
+                VisibleOff()
+
+                pnlStart.Visible = True
+                pnlOpenMicrowave.Visible = False
+            Else
+                Me.BackgroundImage = My.Resources.Microwave_Closed
+                My.Computer.Audio.Play(My.Resources.DoorClose, AudioPlayMode.Background)
+
+                VisibleOn()
+
+                pnlStart.Visible = True
+                pnlOpenMicrowave.Visible = False
+            End If
+        Else
+            Me.BackgroundImage = My.Resources.Microwave_Closed
+            My.Computer.Audio.Play(My.Resources.DoorClose, AudioPlayMode.Background)
+
+            VisibleOn()
+
+            ' pnlStart.Visible = True
+            'pnlCancel.Visible = True
+            pnlOpenMicrowave.Visible = False
+        End If
+    End Sub
+
+    Private Sub tmrTimer_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTimer.Tick
+        If Open = 1 Then
+        Else
+            Start = 1
+            If second > 0 Then
+                second -= 1
+                lblTimer.Text = String.Format("{0:D2}:{1:D2}", minute, second)
+            ElseIf minute > 0 Then
+                minute -= 1
+                second = 59
+                lblTimer.Text = String.Format("{0:D2}:{1:D2}", minute, second)
+
+            Else
+
+                Me.BackgroundImage = My.Resources.Microwave_Closed
+                tmrTimer.Enabled = False
+                lblTimer.Text = "Done"
+
+                VisibleOn()
+
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+                My.Computer.Audio.Play(My.Resources.ButtonBeep, AudioPlayMode.WaitToComplete)
+
+                tmrDelay.Enabled = True
+
+                Start = 0
+            End If
+        End If
+    End Sub
+
+    Private Sub tmrTime_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrTime.Tick
+        If minute = 0 Then
+            If second = 0 Then
+                lblTimer.Text = TimeOfDay
+            End If
+        End If
+    End Sub
+
+    Private Sub tmrDelay_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrDelay.Tick
+        delay += 1
+        If delay = 2 Then
+            tmrDelay.Enabled = False
+            tmrTime.Enabled = True
+            delay = 0
+        End If
+    End Sub
+End Class
